92e59d4ade6a7c37b7db5372c23de6fbde39e5fd
Abdurahman Jafer <95960560+its0beno@users.noreply.github.com>
2025-01-02 17:26:41 +0300

    finished rss feed

--- scraper.go
+++ scraper.go
@@ -12,47 +12,46 @@ import (
 func startScraping(
 	db *database.Queries, 
 	concurrency int,
-	timeBetweenRequest time.Duration){
+	timeBetweenRequest time.Duration) {
 	log.Printf("Starting scraping with %d workers", concurrency)
 	log.Printf("Time between requests: %s", timeBetweenRequest)
-	ticker:=time.NewTicker(timeBetweenRequest)
+	ticker := time.NewTicker(timeBetweenRequest)
 
-for ;; <-ticker.C {
-	feeds,err:= db.GetNextFeedsToFetch( context.Background() , int32(concurrency) )
-	if err != nil {
-		log.Printf("Error getting feeds to scrape %v", err)
-		continue
+	for range ticker.C {
+		feeds, err := db.GetNextFeedsToFetch(context.Background(), int32(concurrency))
+		if err != nil {
+			log.Printf("Error getting feeds to scrape %v", err)
+			continue
+		}
+		wg := &sync.WaitGroup{}
+
+		for _, feed := range feeds {
+			wg.Add(1)
+			go func(feed database.Feed) {
+				defer wg.Done()
+				err := scrapeFeed(db, feed)
+				if err != nil {
+					log.Printf("Error scraping feed %v: %v", feed.ID, err)
+				}
+			}(feed)
+		}
+		wg.Wait()
 	}
-	wg := &sync.WaitGroup{}
+}
 
-	for _,feed := range feeds {
-		wg.Add(1)
-		go scrapeFeed (db,wg,feed){
-			err:= scrapeFeed(feed)
-			if err != nil {
-				log.Printf("Error scraping feed %v: %v", feed.ID, err)
-			}
-		}(feed)
-	}
-	wg.Wait()
-}}
-
-
-func scrapeFeed(wg $sync.WaitGroup,	db *database.Queries,feed database.Feed) {
-	defer wg.Done()
-	_,err:=db.MarkFeedAsFetched(context.Background(),feed.ID)
+func scrapeFeed(db *database.Queries, feed database.Feed) error {
+	_, err := db.MarkFeedAsFetched(context.Background(), feed.ID)
 	if err != nil {
 		log.Printf("Error marking feed as fetched %v: %v", feed.ID, err)
+		return err
 	}
-	rssFeed,err :=  urlTOFeedFollows()
+	rssFeed, err := urlTOFeedFollows("https://www.wagslane.dev/index.xml")
 	if err != nil {
 		log.Printf("Error fetching feed %v: %v", feed.ID, err)
-		return
+		return err
 	}
-	for _,item := range rssFeed.Channel.Item{
+	for _, item := range rssFeed.Channel.Item {
 		log.Printf("Found Post %v", item)
 	}
-
-
-
+	return nil
 }
